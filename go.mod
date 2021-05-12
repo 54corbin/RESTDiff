46429ff7b6f9c0191d195f08f398f4a526332932
54corbin <54corbinchen@gmail.com>
2021-05-12 19:56:49 +0800

    90%

--- go.mod
+++ go.mod
@@ -7,8 +7,10 @@ require (
 	github.com/davidrjenni/reftools v0.0.0-20210213085015-40322ffdc2e4 // indirect
 	github.com/fatih/gomodifytags v1.13.0 // indirect
 	github.com/godoctor/godoctor v0.0.0-20200702010311-8433dcb3dc61 // indirect
+	github.com/google/go-dap v0.5.0 // indirect
 	github.com/josephburnett/jd v1.4.0
 	github.com/josharian/impl v1.0.0 // indirect
+	github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
 	github.com/willf/bitset v1.1.11 // indirect
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
--- main.go
+++ main.go
@@ -57,85 +57,41 @@ func main() {
 	//读取配置文件构造命令对象数组
 	cmds := constructCmds(f)
 
-	fmt.Println("cmd.size=", cmds.Len())
+	fmt.Println("接口总数：", cmds.Len())
 
+	mwg := new(sync.WaitGroup)
+	mwg.Add(cmds.Len())
+	progress := 0
 	for cmd := cmds.Front(); cmd != nil; cmd = cmd.Next() {
 
-		go func(cmdVal *command){
+		progress++
+
+		go func(cmdVal *command,p int32){
+			// fmt.Printf("%+v", cmdVal)
+			defer mwg.Done()
 			wg := new(sync.WaitGroup)
 			wg.Add(2)
 			go doBatchCurl(wg,cmdVal.leftParms,cmdVal.leftCurl,cmdVal.leftResp,cmdVal.leftDuration)
 			go doBatchCurl(wg,cmdVal.rightParms,cmdVal.rightCurl,cmdVal.rightResp,cmdVal.rightDuration)
 			wg.Wait()
+			fmt.Println("准备开始比较...")
 			doBatchCompare(cmdVal.leftResp, cmdVal.rightResp, cmdVal.report)
-		}((cmd.Value).(*command))
-
-		break
-		// fmt.Println(cmdVal)
+			fmt.Printf("%d/%d\n",p,cmds.Len())
+		}((cmd.Value).(*command),int32(progress))
 
 	}
 
-		// break
-		// leftParams := strings.Split(leftReq, "@")
-
-		// if len(leftParams) < 2 {
-		// 	fmt.Print("Wrong cmd fomat at line: ", count)
-		// 	break
-		// }
-
-		// leftParm := leftParams[0]
-		// leftCurl := leftParams[1]
-
-		// // fmt.Printf("p[o]:%s\np[1]:%s", leftParams[0], leftParams[1])
-
-		// paramsPath := fmt.Sprint(filepath.Dir(f.Name()), string(os.PathSeparator), leftParm, ".txt")
-		// fmt.Printf("\nf.Name:%s\nparamsPath:%s", f.Name(), paramsPath)
-
-		// parmFile, err := os.Open(paramsPath)
-		// if err != nil {
-		// 	fmt.Printf("Failed to open file [%s]:%s\n", paramsPath, err)
-		// 	continue
-		// }
-		// defer parmFile.Close()
-
-		// pfBr := bufio.NewReader(parmFile)
-		// for parmLine, err := readLine(pfBr); err == nil || err != io.EOF; parmLine, err = readLine(pfBr) {
-
-		// 	reg, _ := regexp.Compile()
-		// 	fmt.Println(parmLine)
-		// }
-
-		// break
-
-		// //检查是否是合法的curl命令
-		// if strings.HasPrefix(leftReq, "curl") {
-		// 	rightReq, err := readLine(br)
-		// 	//两条curl命令见不能有间隔
-		// 	if err != nil || !strings.HasPrefix(rightReq, "curl") {
-		// 		fmt.Println(leftReq + " >>缺少对应命令")
-		// 		os.Exit(-1)
-		// 	}
-
-		// 	//执行curl 命令 获取接口响应
-		// 	leftResp := curlReq(leftReq)
-		// 	rightResp := curlReq(rightReq)
-
-		// 	count++
-		// 	//构造对比结果文件
-		// 	outPath := fmt.Sprintf("./report%d.txt", count)
-
-		// 	//比较响应结果
-		// 	compare(string(leftResp), string(rightResp), outPath)
-
-		// }
-
+	fmt.Println("mwg wait ....")
+	mwg.Wait()
+	fmt.Println("done....")
 
 }
 
 //批量比较所有返回结果
 func doBatchCompare(left *list.List,right *list.List,report *list.List){
 
-	if left.Len() != right.Len(){
+	fmt.Printf("%+v>====<%+v\n",*left,*right)
+	if nil == left || nil == right || left.Len() != right.Len(){
 		fmt.Println("左右结果集大小不匹配")
 		report.PushBack("左右结果集大小不匹配")
 		return
@@ -143,7 +99,14 @@ func doBatchCompare(left *list.List,right *list.List,report *list.List){
 
 	rightEle := right.Front()
 	for leftEle := left.Front();leftEle != nil;leftEle = leftEle.Next(){
+		if leftEle == nil || rightEle == nil {
+			fmt.Println("结果集为空无法对比")
+			report.PushBack("左右结果集为空无法对比")
+			return
+		}
 		diff := compare(leftEle.Value.(string), rightEle.Value.(string))
+
+		fmt.Print("比对结果",diff)
 		report.PushBack(diff)
 		rightEle = rightEle.Next()
 	}
@@ -164,7 +127,7 @@ func constructCmds(file *os.File) *list.List {
 
 		left, err := readLine(br)
 		if err != nil && err == io.EOF {
-			fmt.Println("end of file....done")
+			// fmt.Println("end of file....done")
 			break
 		}
 
@@ -178,15 +141,27 @@ func constructCmds(file *os.File) *list.List {
 		}
 
 		tmpL := strings.Split(left, "@")
-		tmpR := strings.Split(left, "@")
+		tmpR := strings.Split(right, "@")
 
 		cmd := new(command)
+		cmd.leftParms = list.New()
+		cmd.leftDuration = list.New()
+		cmd.leftResp = list.New()
+
+		cmd.rightParms = list.New()
+		cmd.rightDuration = list.New()
+		cmd.rightResp = list.New()
+
+		cmd.report = list.New()
+
+
 		cmd.leftCurl = tmpL[1]
-		cmd.leftCurl = tmpR[1]
+		cmd.rightCurl = tmpR[1]
 		paramsFilePathL := fmt.Sprint(filepath.Dir(file.Name()), string(os.PathSeparator), tmpL[0], ".txt")
 		paramsFilePathR := fmt.Sprint(filepath.Dir(file.Name()), string(os.PathSeparator), tmpR[0], ".txt")
 		readParms(paramsFilePathL, cmd.leftParms)
-		readParms(paramsFilePathR, cmd.leftParms)
+		readParms(paramsFilePathR, cmd.rightParms)
+		fmt.Printf("leftParm:%+v\nrightParm:%+v\n", *cmd.leftParms,*cmd.rightParms)
 		cmds.PushBack(cmd)
 	}
 	return cmds
@@ -195,14 +170,17 @@ func constructCmds(file *os.File) *list.List {
 //用每个不同的参数依次发送请求
 func doBatchCurl(wg *sync.WaitGroup, parms *list.List,curl string,respList *list.List,durationList *list.List) {
 	defer wg.Done()
+	if nil == parms {
+		fmt.Printf("配置文件有误！！！\n 请求：%s\n参数：%+v\n",curl,*parms)
+		os.Exit(-1)
+
+	}
 	for parmLine := parms.Front(); parmLine != nil && strings.Contains(parmLine.Value.(string), "@@"); parmLine = parmLine.Next() {
 
 		parms := strings.Split((parmLine.Value).(string), "@@")
 
 		for i, parm := range parms {
-
 			curl = strings.Replace(curl, "$$$", parm, i+1)
-
 		}
 
 		leftResp, ld := curlReq(curl)
@@ -213,36 +191,36 @@ func doBatchCurl(wg *sync.WaitGroup, parms *list.List,curl string,respList *list
 
 //将参数文件按行存入list
 func readParms(filePath string, li *list.List) {
+	fmt.Println("parm file path:",filePath)
 
 	f, err := os.Open(filePath)
+	defer f.Close()
 	if err != nil {
-		fmt.Printf("Failed to open file [%s]:%s\n", filePath, err)
+		fmt.Println( err)
 		os.Exit(-1)
 	}
-	defer f.Close()
 
 	pfBr := bufio.NewReader(f)
 	for line, err := readLine(pfBr); err == nil || err != io.EOF; line, err = readLine(pfBr) {
-
 		li.PushBack(line)
 	}
 
 }
 
 //do curl http request
-func curlReq(cmd string) ([]byte,time.Duration) {
+func curlReq(cmd string) (string,time.Duration) {
 
 	start := time.Now()
 	req, err := pcurl.ParseAndRequest(cmd)
 	if err != nil {
 		fmt.Printf("err:%s\n", err)
-		return nil,-1
+		return "",-1
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("err:%s\n", err)
-		return nil,-1
+		return "",-1
 	}
 
 	duration := time.Since(start)
@@ -250,7 +228,7 @@ func curlReq(cmd string) ([]byte,time.Duration) {
 	body, err := ioutil.ReadAll(resp.Body)
 
 	fmt.Printf("\n%s\nVVVVVVVVVVVVVVVVVVVVVV\n%s\n\n", cmd, string(body))
-	return body,duration
+	return string(body),duration
 }
 
 //从reader里读取一行
