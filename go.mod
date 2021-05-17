66808018de7c15919941aa9e05d52d1ae009668b
54corbin <54corbinchen@gmail.com>
2021-05-17 14:38:26 +0800

    json 比较库替换为csdn博客里的代码

--- go.mod
+++ go.mod
@@ -4,15 +4,8 @@ go 1.16
 
 require (
 	github.com/antlabs/pcurl v0.0.7
-	github.com/davidrjenni/reftools v0.0.0-20210213085015-40322ffdc2e4 // indirect
-	github.com/fatih/gomodifytags v1.13.0 // indirect
-	github.com/godoctor/godoctor v0.0.0-20200702010311-8433dcb3dc61 // indirect
-	github.com/google/go-dap v0.5.0 // indirect
+	github.com/antlabs/strsim v0.0.2
 	github.com/josephburnett/jd v1.4.0
-	github.com/josharian/impl v1.0.0 // indirect
-	github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
-	github.com/willf/bitset v1.1.11 // indirect
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
-	golang.org/x/tools v0.1.1 // indirect
 )
--- main.go
+++ main.go
@@ -25,14 +25,14 @@ type command struct {
 	apiName string
 	//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-	leftCurl  string
-	leftParms *list.List
-	leftResp  *list.List
-	leftDuration  *list.List
+	leftCurl     string
+	leftParms    *list.List
+	leftResp     *list.List
+	leftDuration *list.List
 	//====================================
-	rightCurl  string
-	rightParms *list.List
-	rightResp  *list.List
+	rightCurl     string
+	rightParms    *list.List
+	rightResp     *list.List
 	rightDuration *list.List
 
 	//vvvvvvvvvvvvvvvvvvv
@@ -44,11 +44,12 @@ var set = flag.Bool("set", false, "指明待比较json中的数组是有序的")
 func main() {
 
 	var file = flag.String("f", "", "存储curl命令的文件路径")
+	var outPath = flag.String("o", "", "存储运行结果的目录")
 
 	flag.Parse()
 
 	if *file == "" {
-		fmt.Println("请用 -f 指定 存放curl 命令的文件")
+		fmt.Println("请用 -f= 指定 存放curl 命令的文件")
 		os.Exit(-1)
 	}
 	f, err := os.Open(*file)
@@ -57,7 +58,9 @@ func main() {
 		os.Exit(-1)
 	}
 	defer f.Close()
-
+	if *outPath == "" {
+		*outPath = filepath.Dir(f.Name())+string(os.PathSeparator)
+	}
 	//读取配置文件构造命令对象数组
 	cmds := constructCmds(f)
 
@@ -68,19 +71,19 @@ func main() {
 	progress := 0
 	for cmd := cmds.Front(); cmd != nil; cmd = cmd.Next() {
 		progress++
-		go func(cmdVal *command,p int32){
+		go func(cmdVal *command, p int32) {
 			// fmt.Printf("%+v", cmdVal)
 			defer mwg.Done()
 			wg := new(sync.WaitGroup)
 			wg.Add(2)
-			fmt.Println("接口[",cmdVal.apiName,"]开始发起请求...")
-			go doBatchCurl(wg,cmdVal.leftParms,cmdVal.leftCurl,cmdVal.leftResp,cmdVal.leftDuration)
-			go doBatchCurl(wg,cmdVal.rightParms,cmdVal.rightCurl,cmdVal.rightResp,cmdVal.rightDuration)
+			fmt.Println("接口[", cmdVal.apiName, "]开始发起请求...")
+			go doBatchCurl(wg, cmdVal.leftParms, cmdVal.leftCurl, cmdVal.leftResp, cmdVal.leftDuration)
+			go doBatchCurl(wg, cmdVal.rightParms, cmdVal.rightCurl, cmdVal.rightResp, cmdVal.rightDuration)
 			wg.Wait()
-			fmt.Println("接口[",cmdVal.apiName,"]开始比对...")
+			fmt.Println("接口[", cmdVal.apiName, "]开始比对...")
 			doBatchCompare(cmdVal.leftResp, cmdVal.rightResp, cmdVal.report)
-			fmt.Printf("接口[%s] 处理完成...%d/%d\n",cmdVal.apiName,p,cmds.Len())
-		}((cmd.Value).(*command),int32(progress))
+			fmt.Printf("接口[%s] 对比结束...%d/%d\n", cmdVal.apiName, p, cmds.Len())
+		}((cmd.Value).(*command), int32(progress))
 
 	}
 
@@ -89,9 +92,8 @@ func main() {
 	//生成文件
 	for cmd := cmds.Front(); cmd != nil; cmd = cmd.Next() {
 		cmdval := (cmd.Value).(*command)
-		reportPath := fmt.Sprint(filepath.Dir(f.Name()), string(os.PathSeparator),cmdval.apiName )
+		reportPath := filepath.Join(*outPath,cmdval.apiName)
 		os.Mkdir(reportPath, os.ModePerm)
-
 
 		leftResp := cmdval.leftResp.Front()
 		rightResp := cmdval.rightResp.Front()
@@ -100,40 +102,41 @@ func main() {
 		leftDuration := cmdval.leftDuration.Front()
 		rightDuration := cmdval.rightDuration.Front()
 
-		if cmdval.leftResp.Len() != cmdval.rightResp.Len() || cmdval.leftDuration.Len() != cmdval.rightDuration.Len() || cmdval.leftResp.Len() != cmdval.rightDuration.Len(){
-			fmt.Printf("结果集大小不匹配(respL:%d,respR:%d,durationL:%d,durationR:%d) 无法生成报告!!!\n",cmdval.leftResp.Len(),cmdval.rightResp.Len(),cmdval.leftDuration.Len(),cmdval.rightDuration.Len())
+		if cmdval.leftResp.Len() != cmdval.rightResp.Len() || cmdval.leftDuration.Len() != cmdval.rightDuration.Len() || cmdval.leftResp.Len() != cmdval.rightDuration.Len() {
+			fmt.Printf("结果集大小不匹配(respL:%d,respR:%d,durationL:%d,durationR:%d) 无法生成报告!!!\n", cmdval.leftResp.Len(), cmdval.rightResp.Len(), cmdval.leftDuration.Len(), cmdval.rightDuration.Len())
 			os.Exit(-1)
 		}
 
-		for report := cmdval.report.Front();report != nil;report= report.Next(){
+		for report := cmdval.report.Front(); report != nil; report = report.Next() {
 
-			respName := "<"+strings.ReplaceAll(leftParm.Value.(string), " ","_" ) + "><"+strings.ReplaceAll(rightParm.Value.(string)," ","_")+">"
-			respFile := fmt.Sprint(reportPath, string(os.PathSeparator),respName,".txt" )
+			respName := strings.ReplaceAll(leftParm.Value.(string), " ", "_") + "_" + strings.ReplaceAll(rightParm.Value.(string), " ", "_")
+			respFile := filepath.Join(reportPath,respName+".txt")
 
-			reportName := "report_<"+strings.ReplaceAll(leftParm.Value.(string), " ","_" ) + "><"+strings.ReplaceAll(rightParm.Value.(string)," ","_")+">"
-			reportFile := fmt.Sprint(reportPath, string(os.PathSeparator),reportName,".txt" )
+			reportName := "report@" + strings.ReplaceAll(leftParm.Value.(string), " ", "_") + "_" + strings.ReplaceAll(rightParm.Value.(string), " ", "_")
+			reportFile := filepath.Join(reportPath,reportName+".txt")
+			reportFile = strings.ReplaceAll(reportFile, "@@", "")
+			reportFile = strings.ReplaceAll(reportFile, "encode", "")
 
-			fmt.Println("输出报告：",reportFile)
-			rep,err := os.Create(reportFile)
+			fmt.Println("输出报告：", reportFile)
+			rep, err := os.Create(reportFile)
 			defer f.Close()
-			if err != nil{
+			if err != nil {
 				fmt.Println(err)
 			}
 			rep.Write([]byte(report.Value.(string)))
 
-
-			fmt.Println("输出原始接口返回：",respFile)
-			res,err := os.Create(respFile)
+			fmt.Println("输出原始接口返回：", respFile)
+			res, err := os.Create(respFile)
 			defer f.Close()
-			if err != nil{
+			if err != nil {
 				fmt.Println(err)
 			}
 
-			res.Write([]byte(fmt.Sprintf("=======================[耗时：%d ms]=============================\n",leftDuration.Value.(time.Duration).Milliseconds())))
+			res.Write([]byte(fmt.Sprintf("=======================[耗时：%d ms]=============================\n", leftDuration.Value.(time.Duration).Milliseconds())))
 			//格式化json
 			var ltmp bytes.Buffer
-			err = json.Indent(&ltmp,[]byte(leftResp.Value.(string)), "", "\t")
-			if nil != err{
+			err = json.Indent(&ltmp, []byte(leftResp.Value.(string)), "", "\t")
+			if nil != err {
 				fmt.Print("格式化失败")
 				res.Write([]byte(leftResp.Value.(string)))
 			} else {
@@ -141,11 +144,11 @@ func main() {
 				res.Write(ltmp.Bytes())
 			}
 
-			res.Write([]byte(fmt.Sprintf("\n\n=======================[耗时：%d ms]=============================\n",rightDuration.Value.(time.Duration).Milliseconds())))
+			res.Write([]byte(fmt.Sprintf("\n\n=======================[耗时：%d ms]=============================\n", rightDuration.Value.(time.Duration).Milliseconds())))
 			//格式化json
 			var rtmp bytes.Buffer
-			err = json.Indent(&rtmp,[]byte(rightResp.Value.(string)), "", "\t")
-			if nil != err{
+			err = json.Indent(&rtmp, []byte(rightResp.Value.(string)), "", "\t")
+			if nil != err {
 				fmt.Print("格式化失败")
 				res.Write([]byte(rightResp.Value.(string)))
 			} else {
@@ -167,30 +170,46 @@ func main() {
 }
 
 //批量比较所有返回结果
-func doBatchCompare(left *list.List,right *list.List,report *list.List){
+func doBatchCompare(left *list.List, right *list.List, report *list.List) {
 
-	if nil == left || nil == right || left.Len() != right.Len(){
+	if nil == left || nil == right || left.Len() != right.Len() {
 		fmt.Println("左右结果集大小不匹配")
 		report.PushBack("左右结果集大小不匹配")
 		return
 	}
 
 	rightEle := right.Front()
-	for leftEle := left.Front();leftEle != nil;leftEle = leftEle.Next(){
+	for leftEle := left.Front(); leftEle != nil; leftEle = leftEle.Next() {
 		if leftEle == nil || rightEle == nil {
 			fmt.Println("结果集为空无法对比")
 			report.PushBack("左右结果集为空无法对比")
 			return
 		}
-		diff := compare(leftEle.Value.(string), rightEle.Value.(string))
 
-		fmt.Println("比对结果",diff)
-		report.PushBack(diff)
+		lj := new(map[string]interface{})
+		rj := new(map[string]interface{})
+
+		LoadJsonFromString(leftEle.Value.(string), lj)
+		LoadJsonFromString(rightEle.Value.(string), rj)
+
+		diff, hasDiff := JsonCompare(*lj, *rj, -1)
+
+		if hasDiff {
+			fmt.Println("比对结果:", diff)
+			report.PushBack(diff)
+		} else {
+
+			diff = "No differents"
+			fmt.Println("比对结果:", diff)
+			report.PushBack(diff)
+		}
 		rightEle = rightEle.Next()
+
+		// diff := compare(leftEle.Value.(string), rightEle.Value.(string))
+
 	}
 
 }
-
 
 //读取文件构造请求/比对 对象数组
 func constructCmds(file *os.File) *list.List {
@@ -199,7 +218,7 @@ func constructCmds(file *os.File) *list.List {
 
 	br := bufio.NewReader(file)
 
-	for count := 0; ;  {
+	for count := 0; ; {
 
 		left, err := readLine(br)
 		count++
@@ -213,7 +232,7 @@ func constructCmds(file *os.File) *list.List {
 		}
 		count++
 		right, err := readLine(br)
-		if (err != nil && err == io.EOF) || !strings.Contains(right, "curl") || 2 != strings.Count(left, "@")|| 2 != strings.Count(right, "@") {
+		if (err != nil && err == io.EOF) || !strings.Contains(right, "curl") || 2 != strings.Count(left, "@") || 2 != strings.Count(right, "@") {
 			fmt.Println(left, " >>格式不正确 行号：", count)
 			os.Exit(-1)
 		}
@@ -249,20 +268,20 @@ func constructCmds(file *os.File) *list.List {
 }
 
 //用每个不同的参数依次发送请求
-func doBatchCurl(wg *sync.WaitGroup, parms *list.List,curl string,respList *list.List,durationList *list.List) {
+func doBatchCurl(wg *sync.WaitGroup, parms *list.List, curl string, respList *list.List, durationList *list.List) {
 	defer wg.Done()
 	if nil == parms {
-		fmt.Printf("配置文件有误！！！\n 请求：%s\n参数：%+v\n",curl,*parms)
+		fmt.Printf("配置文件有误！！！\n 请求：%s\n参数：%+v\n", curl, *parms)
 		os.Exit(-1)
 
 	}
-	for parmLine := parms.Front(); parmLine != nil && strings.Contains(parmLine.Value.(string), "@@"); parmLine = parmLine.Next() {
+	for parmLine := parms.Front(); parmLine != nil; parmLine = parmLine.Next() {
 
 		parms := strings.Split((parmLine.Value).(string), "@@")
 
 		tmpCurl := curl
 		for i, parm := range parms {
-			if strings.HasPrefix(parm, "encode:"){
+			if strings.HasPrefix(parm, "encode:") {
 				parm = strings.TrimPrefix(parm, "encode:")
 				tmpCurl = strings.Replace(tmpCurl, "$$$", url.QueryEscape(parm), i+1)
 				continue
@@ -279,46 +298,49 @@ func doBatchCurl(wg *sync.WaitGroup, parms *list.List,curl string,respList *list
 
 //将参数文件按行存入list
 func readParms(filePath string, li *list.List) {
-	fmt.Println("读取参数文件：",filePath)
+	fmt.Println("读取参数文件：", filePath)
 
 	f, err := os.Open(filePath)
 	defer f.Close()
 	if err != nil {
-		fmt.Println( err)
+		fmt.Println(err)
 		os.Exit(-1)
 	}
 
 	pfBr := bufio.NewReader(f)
 	for line, err := readLine(pfBr); err == nil || err != io.EOF; line, err = readLine(pfBr) {
+		if line == "" {
+			continue
+		}
 		li.PushBack(line)
 	}
 
 }
 
 //do curl http request
-func curlReq(cmd string) (string,time.Duration) {
+func curlReq(cmd string) (string, time.Duration) {
 
 	start := time.Now()
 	req, err := pcurl.ParseAndRequest(cmd)
 	if err != nil {
-		fmt.Println(err)
-		es :=fmt.Sprintf("%v", err)
-		return es,0
+		fmt.Printf("\n%s\n\t\tVVVVVVVVVVVVVVVVVVVVVV\n\t\t\t\tVVV\n%s\n\n", cmd, err)
+		es := fmt.Sprintf("%v", err)
+		return es, 0
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		es :=fmt.Sprintf("%v", err)
-		return es,0
+		fmt.Printf("\n%s\n\t\tVVVVVVVVVVVVVVVVVVVVVV\n\t\t\t\tVVV\n%s\n\n", cmd, err)
+		es := fmt.Sprintf("%v", err)
+		return es, 0
 	}
 
 	duration := time.Since(start)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 
-	fmt.Printf("\n%s\nVVVVVVVVVVVVVVVVVVVVVV\n%s\n\n", cmd, string(body))
-	return string(body),duration
+	fmt.Printf("\n%s\n\t\tVVVVVVVVVVVVVVVVVVVVVV\n\t\t\t\tVVV\n%s\n\n", cmd, string(body))
+	return string(body), duration
 }
 
 //从reader里读取一行
@@ -336,13 +358,13 @@ func compare(lj string, rj string) string {
 
 	a, err := jd.ReadJsonString(lj)
 	if nil != err {
-		es := fmt.Sprintf("%s\n\nerror：%s\n",lj, err)
+		es := fmt.Sprintf("%s\n\nerror：%s\n", lj, err)
 		fmt.Println(es)
 		return es
 	}
 	b, err2 := jd.ReadJsonString(rj)
 	if nil != err2 {
-		es := fmt.Sprintf("%s\n\nerror：%s\n", rj,err)
+		es := fmt.Sprintf("%s\n\nerror：%s\n", rj, err)
 		fmt.Println(es)
 		return es
 	}
